0eeabb80fa51617600d2e49a2ec27d04fe3c8fce
arminv <arminv@users.noreply.github.com>
2020-12-24 21:54:40 -0500

    End of Section 10 (53:00)

--- src/products/models.py
+++ src/products/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='This is really cool!Wow')
+    # Note `max_length` is a required arg!
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=1000)
+    summary = models.TextField()
