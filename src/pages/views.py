3e698793b680a0ac0187553ce85eb41b0e1044ea
arminv <arminv@users.noreply.github.com>
2020-12-26 02:06:18 -0500

    End of Section 13 (mark 1:10:23)

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,5 +5,18 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    # Note we can see the request object:
+    print(request)
+    # Note we can see the user who is sending the request:
+    print(request.user)
     return HttpResponse("<h1>Hello World!</h1>")  # string of HTML code
+
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")  # string of HTML code
+
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")  # string of HTML code
