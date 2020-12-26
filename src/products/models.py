0e654e57c13b3932b35afbca21313e45c4a8d3fd
arminv <arminv@users.noreply.github.com>
2020-12-26 01:45:35 -0500

    End of Section 11 (mark: 1:00:00)

--- src/products/models.py
+++ src/products/models.py
@@ -5,8 +5,11 @@ from django.db import models
 
 
 class Product(models.Model):
-    # Note `max_length` is a required arg!
+    # Note `max_length` is a required arg:
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=1000)
-    summary = models.TextField()
+    # Note `blank` only changes the way the field shows/renders, it has nothing to do with the db (whereas `null` has to do with db instead):
+    summary = models.TextField(blank=False, null=False)
+    # Note `featured` is added later on, so we have two options: null=True and/or default=True
+    featured = models.BooleanField()
