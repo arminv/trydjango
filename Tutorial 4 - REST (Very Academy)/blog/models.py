a806c6bd4a2e14c772af6e6411999a73c9db783b
arminv <arminv@users.noreply.github.com>
2020-12-28 15:56:33 -0500

    Progress (mark 46:00)

--- Tutorial 4 - REST (Very Academy)/blog/models.py
+++ Tutorial 4 - REST (Very Academy)/blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    # Note, we create a model manager that will help us filter and return data (instead of returning all data available in db!):
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    category = models.ForeignKey(
+        # Note, PROTECT means if user deletes a category, posts will be unaffected:
+        Category, on_delete=models.PROTECT, default=1
+    )
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        # Note, CASCADE means if we delete a user, the posts for that user will also be deleted:
+        User, on_delete=models.CASCADE, related_name='blog_posts'
+    )
+    status = models.CharField(
+        max_length=10, choices=options, default='published'
+    )
