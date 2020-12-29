c03ed3629e67f7a8150ccf34065dbd13f785991d
arminv <arminv@users.noreply.github.com>
2020-12-29 00:18:50 -0500

    Progress (mark 55:00)

---
+++ Tutorial 4 - REST (Very Academy)/blog_api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    # Note, `postobjects` is the custom manager we created in the models:
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+    pass
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    pass
