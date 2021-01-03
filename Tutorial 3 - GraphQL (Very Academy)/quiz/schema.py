ae67fa84c2138bebf89cff34eaa31828e1968d75
arminv <arminv@users.noreply.github.com>
2021-01-02 19:52:23 -0500

    End of Part 3

--- Tutorial 3 - GraphQL (Very Academy)/quiz/schema.py
+++ Tutorial 3 - GraphQL (Very Academy)/quiz/schema.py
@@ -40,17 +40,47 @@ class Query(graphene.ObjectType):
         return Answer.objects.filter(question=id)
 
 
+# 1) CREATE/ADD MUTATION:
+# class CategoryMutation(graphene.Mutation):
+#     class Arguments:
+#         name = graphene.String(required=True)
+
+#     category = graphene.Field(CategoryType)
+
+#     @classmethod
+#     def mutate(cls, root, info, name):
+#         category = Category(name=name)
+#         category.save()
+#         return CategoryMutation(category=category)
+
+# 2) UPDATE MUTATION:
 class CategoryMutation(graphene.Mutation):
     class Arguments:
+        id = graphene.ID()
         name = graphene.String(required=True)
 
     category = graphene.Field(CategoryType)
 
     @classmethod
-    def mutate(cls, root, info, name):
-        category = Category(name=name)
+    def mutate(cls, root, info, name, id):
+        category = Category.objects.get(id=id)
+        category.name = name
         category.save()
         return CategoryMutation(category=category)
+
+# 3) DELETE MUTATION:
+# class CategoryMutation(graphene.Mutation):
+#     class Arguments:
+#         id = graphene.ID()
+
+#     category = graphene.Field(CategoryType)
+
+#     @classmethod
+#     def mutate(cls, root, info, id):
+#         category = Category.objects.get(id=id)
+#         category.delete()
+# #  Note, we don't need to return anything as it is only a delete operation:
+#         return
 
 
 class Mutation(graphene.ObjectType):
